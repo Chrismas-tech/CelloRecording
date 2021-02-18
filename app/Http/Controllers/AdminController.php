dcd030ae9db13420dbd2f621e6b49c0c5288ecc7
Chris <lucianichristophe0@gmail.com>
2021-02-18 18:35:30 +0100

    admin_connection problem

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -30,7 +30,7 @@ class AdminController extends Controller
     }
 
     public function verify_admin_connection() {
-        return redirect('connection_admin');
+        return redirect('admin_connection');
     }
 
     public function page_admin()
