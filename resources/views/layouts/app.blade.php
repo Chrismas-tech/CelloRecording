aa5d25c6bfbaedde836dc6c6a8c59454e760fe4e
Chris <lucianichristophe0@gmail.com>
2021-03-15 13:14:54 +0100

    Current year footer

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -37,7 +37,7 @@
 
     <footer class="flex justify-center p-8 text-xl text-white bg-gray-500 border border-gray-500 nav_button_font">
         <div>
-            <h3>Copyright &copy; 2020 Christophe Luciani, all Rights Reserved</h3>
+            <h3>Copyright &copy; {{date('Y')}} Christophe Luciani, all Rights Reserved</h3>
         </div>
     </footer>
     </div>
