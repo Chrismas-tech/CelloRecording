e5010742b73f29de7920d44108dd09362486573d
Chris <lucianichristophe0@gmail.com>
2021-03-15 17:28:14 +0100

    style css welcome page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -91,7 +91,7 @@
         <div class="bg_about_me px-5 py-16">
             <div class="xl:container mx-auto border-black rounded">
                 <div id="aboutme" class="mq_about text-6xl pb-3 redressed_font">
-                    <h1 class="text-white nav_button_font"">About Me</h1>
+                    <h1 class="text-white nav_button_font mq_font_size_h1">About Me</h1>
                 </div>
 
                 <div class="flex text-xl rounded ">
@@ -133,7 +133,7 @@
 
                 <div>
                     <div id="hdiw" class="text-6xl text-gray-700 pb-3 rounded redressed_font">
-                        <h1 class="text-white nav_button_font"">How does it work ?</h1>
+                        <h1 class="text-white nav_button_font mq_font_size_h1">How does it work ?</h1>
                     </div>
 
                     <div class="mq_padding_presentation_text p-8 text-xl bg-white karma rounded border border-gray-300">
@@ -167,7 +167,7 @@
             <div class="xl:container mx-auto border-black rounded">
 
                 <div id="record_env" class="text-6xl pb-3 text-white rounded redressed_font">
-                    <h1 class="nav_button_font">Recording Environment</h1>
+                    <h1 class="nav_button_font mq_font_size_h1">Recording Environment</h1>
                 </div>
 
                 <div class="flex mq_padding_presentation_text text-xl karma">
@@ -264,7 +264,7 @@
         <div class="bg-gray-600 px-5 py-16">
             <div class="xl:container mx-auto border-black rounded">
                 <div id="pricing" class="pb-3 text-6xl text-white rounded redressed_font">
-                    <h1 class="nav_button_font">Pricing</h1>
+                    <h1 class="nav_button_font mq_font_size_h1">Pricing</h1>
                 </div>
 
                 <div class="mq_padding_presentation_text p-8 text-xl karma bg-white rounded">
@@ -283,7 +283,7 @@
         <div>
 
             <div id="social_media" class="flex justify-center text-6xl pb-3 text-white rounded ">
-                <h1>Social Medias</h1>
+                <h1 class="nav_button_font mq_font_size_h1">Social Medias</h1>
             </div>
 
             <div class="mq_social_icon flex justify-center">
