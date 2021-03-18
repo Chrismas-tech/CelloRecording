8363ceaec41693825e19a91ea1632a173070d590
Chris <lucianichristophe0@gmail.com>
2021-03-18 11:42:32 +0100

    responsive admin menu navbar

--- resources/views/components/nav_hamburger_admin.blade.php
+++ resources/views/components/nav_hamburger_admin.blade.php
@@ -11,7 +11,7 @@ $not_active_admin ="border-green-200";
             class="relative p-5  border-b-2 hover:border-green-500 {{ Route::currentRouteName() == 'dashboard_admin' ? $active_admin : $not_active_admin }}">
             Admin Dashboard
             <div class="absolute bottom-3 right-3">
-                <img src="{{ asset('img/profile.png') }}" alt="" class="w-12">
+                <img src="{{ asset('img/profile.png') }}" alt="" class="w-8">
             </div>
         </li>
     </a>
@@ -20,7 +20,7 @@ $not_active_admin ="border-green-200";
             class="relative p-5  border-b-2 hover:border-green-500  {{ Route::currentRouteName() == 'list_conversation_admin' ? $active_admin : $not_active_admin }}">
             Conversations with Clients
             <div class="absolute bottom-3 right-3 flex items-center">
-                <img src="{{ asset('img/messages_icon.png') }}" alt="" class="w-12">
+                <img src="{{ asset('img/messages_icon.png') }}" alt="" class="w-8">
                 @if (App\Http\Controllers\AdminController::notifications() > 0)
                     <div class="flex items-center justify-center text-black">
                         <p class="text-sm font-bold">
@@ -35,7 +35,7 @@ $not_active_admin ="border-green-200";
             class="relative p-5  border-b-2 hover:border-green-500 {{ Route::currentRouteName() == 'quotes_sent' ? $active_admin : $not_active_admin }}">
             Your Quotes
 
-            <div class="absolute bottom-3 right-3">
+            <div class="flex absolute bottom-3 right-3">
                 <img src="{{ asset('img/quotes_icon.png') }}" alt="" class="w-10">
                 @if (App\Http\Controllers\AdminController::quotes_notifications() > 0)
                     <div class="flex items-center justify-center text-black">
@@ -53,7 +53,7 @@ $not_active_admin ="border-green-200";
             My Orders
 
             <div class="absolute bottom-3 right-3 flex items-center">
-                <img src="{{ asset('img/order_icon.png') }}" alt="" class="w-12">
+                <img src="{{ asset('img/order_icon.png') }}" alt="" class="w-8">
                 @if (App\Http\Controllers\AdminController::order_notifications() > 0)
                     <div class=" flex items-center justify-center text-black">
                         <p class="text-sm font-bold">
@@ -70,7 +70,7 @@ $not_active_admin ="border-green-200";
             class=" relative p-5 md:mr-8 border-green-200 border-b-2 hover:border-b-2 hover:border-green-500">
             <button type="submit" class=" focus:outline-none">Logout</button>
             <div class="absolute bottom-3 right-3 flex items-center">
-                <img src="{{ asset('img/shutdown.png') }}" alt="" class="w-12">
+                <img src="{{ asset('img/shutdown.png') }}" alt="" class="w-8">
 
             </div>
         </li>
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -35,7 +35,7 @@
             @yield('content')
         </main>
 
-    <footer class="flex justify-center p-8 text-xl text-white bg-gray-500 border border-gray-500 nav_button_font">
+    <footer class="flex justify-center p-4 sm:p-8 text-xl text-white bg-gray-500 border border-gray-500 nav_button_font">
         <div>
             <h3>Copyright &copy; {{date('Y')}} Christophe Luciani, all Rights Reserved</h3>
         </div>
