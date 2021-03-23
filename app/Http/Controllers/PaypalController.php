afb345ac301c6afed4e012784c200e45765ecf3a
Chris <lucianichristophe0@gmail.com>
2021-03-23 23:30:52 +0100

    debugging paypal_live problem api context

--- app/Http/Controllers/PaypalController.php
+++ app/Http/Controllers/PaypalController.php
@@ -105,8 +105,8 @@ class PaypalController extends Controller
             ->setRedirectUrls($redirectUrls)
             ->setTransactions(array($transaction));
 
+        dd($payment->create($this->apiContext));
         $payment->create($this->apiContext);
-        dd('yolo');
 
         return redirect($payment->getApprovalLink());
     }
